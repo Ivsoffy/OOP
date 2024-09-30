34f03cef4c832ce651868ac2331c536442a81514
Errok Elenor <errokele@student.21-school.ru>
2024-09-30 13:27:36 +0700

    idk

--- Task_1_1_1/src/main/java/ru/nsu/ivchenko/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/ivchenko/HeapSort.java
@@ -5,78 +5,83 @@ import java.util.Random;
 
 public class HeapSort {
 
-  public void heapsort(int arr[]) {
-    int N = arr.length;
-
-    for (int i = N / 2 - 1; i >= 0; i--) {
-      heapify(arr, N, i);
-    }
-
-    for (int i = N - 1; i > 0; i--) {
-      int tmp = arr[0];
-      arr[0] = arr[i];
-      arr[i] = tmp;
-
-      heapify(arr, i, 0);
-    }
-  }
-
   /**
-   * Function that swaps elems in binary tree if parent more/less than child.
+   * Function that sorts an array.
    *
-   * @param arr — heap's array;
-   * @param i   — current index in array.
-   * @param N   — len of array.
+   * @param arr — array;
    */
-  void heapify(int arr[], int N, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
+    public void heapsort(int[] arr) {
+        int N = arr.length;
 
-    if (right < N && arr[right] > arr[largest]) {
-      largest = right;
-    }
-
-    if (left < N && arr[left] > arr[largest]) {
-      largest = left;
-    }
-
-    if (largest != i) {
-      int tmp = arr[i];
-      arr[i] = arr[largest];
-      arr[largest] = tmp;
-
-      heapify(arr, N, largest);
-    }
-  }
-
-  public static void main(String[] args) {
-        /*HeapSort hs = new HeapSort();
-        Random random = new Random();
-
-        int size = 10000;
-        long[] sizes = new long[10];
-        long[] times = new long[10];
-
-        for (int i = 0; i < 10; i++) {
-            sizes[i] = size;
-            int[] arr = new int[size];
-
-            for (int j = 0; j < size; j++) {
-                arr[j] = random.nextInt();
-            }
-
-            long startTime = System.nanoTime();
-            hs.heapsort(arr);
-            long endTime = System.nanoTime();
-
-            times[i] = endTime - startTime;
-            System.out.println("Размер массива: " + size + ", время: " + times[i] + " наносек");
-            size = size +20000;
+        for (int i = N / 2 - 1; i >= 0; i--) {
+          heapify(arr, N, i);
         }
 
-        // Вывод массивов с результатами
-        System.out.println("Размеры массивов: " + Arrays.toString(sizes));
-        System.out.println("Время выполнения (нс): " + Arrays.toString(times));*/
-  }
+        for (int i = N - 1; i > 0; i--) {
+          int tmp = arr[0];
+          arr[0] = arr[i];
+          arr[i] = tmp;
+
+          heapify(arr, i, 0);
+        }
+    }
+
+    /**
+     * Function that swaps elems in binary tree if parent more/less than child.
+     *
+     * @param arr — heap's array;
+     * @param i   — current index in array.
+     * @param N   — len of array.
+     */
+    void heapify(int[] arr, int N, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (right < N && arr[right] > arr[largest]) {
+          largest = right;
+        }
+
+        if (left < N && arr[left] > arr[largest]) {
+          largest = left;
+        }
+
+        if (largest != i) {
+          int tmp = arr[i];
+          arr[i] = arr[largest];
+          arr[largest] = tmp;
+
+          heapify(arr, N, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+          /*HeapSort hs = new HeapSort();
+          Random random = new Random();
+
+          int size = 10000;
+          long[] sizes = new long[10];
+          long[] times = new long[10];
+
+          for (int i = 0; i < 10; i++) {
+              sizes[i] = size;
+              int[] arr = new int[size];
+
+              for (int j = 0; j < size; j++) {
+                  arr[j] = random.nextInt();
+              }
+
+              long startTime = System.nanoTime();
+              hs.heapsort(arr);
+              long endTime = System.nanoTime();
+
+              times[i] = endTime - startTime;
+              System.out.println("Размер массива: " + size + ", время: " + times[i] + " наносек");
+              size = size +20000;
+          }
+
+          // Вывод массивов с результатами
+          System.out.println("Размеры массивов: " + Arrays.toString(sizes));
+          System.out.println("Время выполнения (нс): " + Arrays.toString(times));*/
+    }
 }
