dd16df0e62e5bc780e3e4fb8c404f28a98946f64
Ivsoffy <Ivsoffy@users.noreply.github.com>
2024-09-30 05:17:11 +0000

    Deploying to gh-pages from @ Ivsoffy/OOP@e602038938fabc45ebab5b6ae9d976a4ec91a342 🚀

--- Task_1_1_1/package-search-index.js
+++ Task_1_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.example"},{"l":"ru.nsu.ivchenko"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.ivchenko"}]
